deed37ca6723f461ff9fb55f78f330a17142a7c8
Mustafa Zamani <zamani.mustafa91@gmail.com>
2022-09-09 19:51:32 +0200

    17-switch vs if/else

---
+++ Codes_with_gio/1-17Switch_VS_if_or_else.php
@@ -0,0 +1,82 @@
+<?php
+
+$paymentStatus = 'paid';
+
+swith($paymentStatus)
+{
+    case 'Paid':
+    case 'paid': 
+        echo "Paid";
+        break;
+    case 'declined':
+        echo 'declined';
+        break;
+    case 'Pending':
+        echo 'Pending';
+        break;
+    default:
+        echo 'unknown payment status';
+}
+
+#swithc statement do loose comprison 
+$paymentStatuses = ['paid', 'Paid', 'declined'];
+
+foreach($paymentStatuses as $paymentStatus){
+swith($paymentStatus)
+{
+    case 'Paid':
+    case 'paid': 
+        echo "Paid";
+        break 2;
+
+    case 'declined':
+        continue 2;
+        echo 'declined';
+        break;
+
+    case 'Pending':
+        echo 'Pending';
+        break;
+    default:
+        echo 'unknown payment status';
+}
+echo '<br />';
+}
+
+#usage example 
+function x()
+{
+    sleep (3);
+    echo 'Done'."<br />";
+    return 3;
+}
+
+if($x()===1){
+    echo 1;
+}elseif($x()===2){
+    echo 2;
+}elseif($x()===3){
+    echo 3;
+}else{
+    echo 4;
+}
+
+switch(x()){
+    {
+        case 1:
+        case '1': 
+            echo 1;
+            break 2;
+    
+        case 2:
+            continue 2;
+            echo 2;
+            break;
+    
+        case 3:
+            echo 3;
+            break;
+        default:
+            echo 'unknown umber';
+    }
+}
