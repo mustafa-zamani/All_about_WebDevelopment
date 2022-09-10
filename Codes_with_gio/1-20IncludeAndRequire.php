7d12c2f492927a7d8be3a3483b760b6fbeec8418
Mustafa Zamani <zamani.mustafa91@gmail.com>
2022-09-10 08:26:38 +0200

    1-20

---
+++ Codes_with_gio/1-20IncludeAndRequire.php
@@ -0,0 +1,6 @@
+<?php
+
+/**require / require_once / include / include_once */
+// if file not exist include gives warning while it doesn't stop execution 
+// if file not exist require will start an error and stop execution.
+
