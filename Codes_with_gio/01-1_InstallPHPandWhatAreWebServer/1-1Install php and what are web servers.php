56daa14252764fa7eefbd6db263d014a18708d91
mustafa-zamani <zamani.mustafa91@gmail.com>
2022-09-11 11:21:20 +0200

    updaing missing files and directory

---
+++ Codes_with_gio/01-1_InstallPHPandWhatAreWebServer/1-1Install php and what are web servers.php
@@ -0,0 +1,7 @@
+<?php
+// what is web server
+// https://www.techtarget.com/whatis/definition/Web-server
+// local webserver app like
+// xampp or wamp or lamp
+// most common issue with starting xamp 
+// is using same port for two app services
