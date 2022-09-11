1801c905f5b40327e7a555e723b4ce725915084b
mustafa-zamani <zamani.mustafa91@gmail.com>
2022-09-11 11:46:41 +0200

    PHP SYNTAX

---
+++ Codes_with_gio/01-02_PHP_Syntax/01-02_PHP_syntax.php
@@ -0,0 +1,28 @@
+<?php
+// only close your php tag if you are mixing it with html or other language
+echo "hello world";
+// ; forget will occur parsing error
+// ; is not required in last statement with php file
+// print returen 1 can work with statements 
+// echo doesn't 
+echo print "hello world";
+// print echo "hello world"; #it doesn't work 
+// echo also can append text with ,
+echo 'hello', ' ', 'world';
+// print 'hello', ' ', 'world'; # doen'st work
+// variables can start with
+//  a-z A-Z _ 
+// not number or special character
+// but can have in other part of var except specail char
+// and can not be assigned to $this
+// valid var $_abd123 or $adfa_123;
+$firstName = 'Gio';
+echo 'Hello $firstName'.'<br />';
+echo "Hello $firstName".'<br />';
+echo "Hello ${firstName}".'<br />';
+echo "Hello {$firstName}".'<br />';
+/*
+<h1> <?php echo "hello world"; ?> </h1>
+or 
+<h1> <?= "hello World" ?> </h1> 
+*?
